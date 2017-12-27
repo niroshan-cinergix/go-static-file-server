d1051a493fa203533771e77b3c3dc101301ee9e2
Niroshan Madusanka <niroshan@cinergix.com>
2017-12-27 12:43:33 +0530

    Adding custom 404 handler, not what I expected

--- static.go
+++ static.go
@@ -1,12 +1,37 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"path"
 )
 
+func notFound(response http.ResponseWriter, request *http.Request) {
+	body, _ := ioutil.ReadFile("./pages/404.html")
+	fmt.Fprintf(response, string(body))
+}
+
+// func requestHandler(response http.ResponseWriter, request *http.Request) http.Handler {
+// }
+
+func staticFileServer(fileSystem http.FileSystem) http.Handler {
+	fileServer := http.FileServer(fileSystem)
+	return http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
+		_, err := fileSystem.Open(path.Clean(request.URL.Path))
+		if os.IsNotExist(err) {
+			notFound(response, request)
+		} else {
+			fileServer.ServeHTTP(response, request)
+		}
+	})
+}
+
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./pages")))
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		panic(err)
-	}
+	// http.Handle("/", http.FileServer(http.Dir("./pages")))
+	// if err := http.ListenAndServe(":8080", nil); err != nil {
+	// 	panic(err)
+	// }
+	http.ListenAndServe(":8080", staticFileServer(http.Dir("./pages")))
 }
