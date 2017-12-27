8a478b3add58fa272b47ec19aeeedea9e813091b
Niroshan Madusanka <niroshan@cinergix.com>
2017-12-27 13:47:43 +0530

    Proper Notfound Handler

--- static.go
+++ static.go
@@ -1,37 +1,28 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"net/http"
 	"os"
 	"path"
 )
 
-func notFound(response http.ResponseWriter, request *http.Request) {
-	body, _ := ioutil.ReadFile("./pages/404.html")
-	fmt.Fprintf(response, string(body))
+func targetFile(request *http.Request) string {
+	fileOnRequest := request.URL.Path
+	targetName := path.Join("./pages", fileOnRequest)
+	f, err := os.Open(targetName)
+	if err != nil {
+		return "./pages/404.html"
+	}
+	defer f.Close()
+	return targetName
 }
 
-// func requestHandler(response http.ResponseWriter, request *http.Request) http.Handler {
-// }
-
-func staticFileServer(fileSystem http.FileSystem) http.Handler {
-	fileServer := http.FileServer(fileSystem)
+func staticFileServer() http.Handler {
 	return http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
-		_, err := fileSystem.Open(path.Clean(request.URL.Path))
-		if os.IsNotExist(err) {
-			notFound(response, request)
-		} else {
-			fileServer.ServeHTTP(response, request)
-		}
+		http.ServeFile(response, request, targetFile(request))
 	})
 }
 
 func main() {
-	// http.Handle("/", http.FileServer(http.Dir("./pages")))
-	// if err := http.ListenAndServe(":8080", nil); err != nil {
-	// 	panic(err)
-	// }
-	http.ListenAndServe(":8080", staticFileServer(http.Dir("./pages")))
+	http.ListenAndServe(":8080", staticFileServer())
 }
